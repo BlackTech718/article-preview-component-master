f2513971fff0d88b539afd72dd6940958a17e184
BlackTeck718 <nandoughcorp@live.com>
2020-10-25 23:36:47 -0400

    complete_on_free_time_

--- styles/app.js
+++ styles/app.js
@@ -1,10 +1,24 @@
 // Show Social box when share button is clicked
-var share_btn = document.getElementsByClassName("grid-container__share-btn");
-var share_box = document.getElementsByClassName("grid-container__social");
-share_btn[0].children[0].addEventListener('click',function(){
-    if(share_box[0].classList.contains('open')){
-        share_box[0].classList.remove('open')
+var share_btn = document.getElementsByClassName("grid-container__share-btn")[0];
+var share_box = document.getElementsByClassName("grid-container__social")[0];
+var contact_box = document.getElementsByClassName("grid-container__contact")[0];
+var share_small = document.getElementsByClassName("grid-container__share-btn")[1]
+
+share_btn.addEventListener('click',function(){
+    if(contact_box.classList.contains('close')){
+        contact_box.classList.remove('close');
+        share_box.classList.add('close');
     }else{
-        share_box[0].classList.add('open');
+        contact_box.classList.add('close');
+        share_box.classList.remove('close');
+    }
+});
+share_small.addEventListener('click',function(){
+    if(contact_box.classList.contains('close')){
+        contact_box.classList.remove('close');
+        share_box.classList.add('close');
+    }else{
+        contact_box.classList.add('close');
+        share_box.classList.remove('close');
     }
 });
