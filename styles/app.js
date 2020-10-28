55b2e52f1d359e74ece3d27de4b17a6d077e5ff6
BlackTeck718 <nandoughcorp@live.com>
2020-10-27 20:47:03 -0400

    JavaComplete_Needs_Styling

--- styles/app.js
+++ styles/app.js
@@ -3,25 +3,28 @@ var share_btn = document.getElementsByClassName("grid-container__share-btn")[0];
 var share_box = document.getElementsByClassName("grid-container__social")[0];
 var contact_box = document.getElementsByClassName("grid-container__contact")[0];
 var share_small = document.getElementsByClassName("grid-container__share-btn")[1]
-
-
+var arrow = document.getElementsByClassName('arrow');
 
 
 share_btn.addEventListener('click',function(){
     if(contact_box.classList.contains('close')){
         contact_box.classList.remove('close');
+        arrow[0].classList.add('close');
         share_box.classList.add('close');
     }else{
         contact_box.classList.add('close');
+        arrow[0].classList.remove('close');
         share_box.classList.remove('close');
     }
 });
 share_small.addEventListener('click',function(){
     if(contact_box.classList.contains('close')){
         contact_box.classList.remove('close');
+        arrow[0].classList.add('close');
         share_box.classList.add('close');
     }else{
         contact_box.classList.add('close');
+        arrow[0].classlist.remove('close');
         share_box.classList.remove('close');
     }
 });
