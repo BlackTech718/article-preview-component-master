8ca06388464b59e4de713adf739bddb31b9891e4
BlackTeck718 <nandoughcorp@live.com>
2020-10-28 20:41:27 -0400

    js_done

--- styles/app.js
+++ styles/app.js
@@ -5,27 +5,52 @@ var contact_box = document.getElementsByClassName("grid-container__contact")[0];
 var share_small = document.getElementsByClassName("grid-container__share-btn")[1];
 var arrow = document.getElementsByClassName('arrow')[0];
 
-
-
-share_btn.addEventListener('click',function(){
-    if(contact_box.classList.contains('close')){
-        contact_box.classList.remove('close');
-        share_box.classList.add('close');
-        arrow.classList.add('close');
-    }else{
-        contact_box.classList.add('close');
-        share_box.classList.remove('close');
-        arrow.classList.remove('close');
+function myFunction(x) {
+    if (x.matches) { // If media query matches
+     share_btn.addEventListener('click',function(){
+        if(share_box.classList.contains('close')){
+            share_box.classList.remove('close');
+            arrow.classList.remove('close');
+        }else{
+            share_box.classList.add('close');
+            arrow.classList.add('close');
+        }
+    });
+     share_small.addEventListener('click',function(){
+        if(share_box.classList.contains('close')){
+            share_box.classList.remove('close');
+            arrow.classList.remove('close');
+        }else{
+            share_box.classList.add('close');
+            arrow.classList.add('close');
+        }
+    });   
+    } else {
+        share_btn.addEventListener('click',function(){
+            if(contact_box.classList.contains('close')){
+                contact_box.classList.remove('close');
+                share_box.classList.add('close');
+                arrow.classList.add('close');
+            }else{
+                contact_box.classList.add('close');
+                share_box.classList.remove('close');
+                arrow.classList.remove('close');
+            }
+        });
+        share_small.addEventListener('click',function(){
+            if(contact_box.classList.contains('close')){
+                contact_box.classList.remove('close');
+                share_box.classList.add('close');
+                arrow.classList.add('close');
+            }else{
+                contact_box.classList.add('close');
+                share_box.classList.remove('close');
+                arrow.classList.remove('close');
+            }
+        });
     }
-});
-share_small.addEventListener('click',function(){
-    if(contact_box.classList.contains('close')){
-        contact_box.classList.remove('close');
-        share_box.classList.add('close');
-        arrow.classList.add('close');
-    }else{
-        contact_box.classList.add('close');
-        share_box.classList.remove('close');
-        arrow.classList.remove('close');
-    }
-});
+}
+var x = window.matchMedia("(min-width: 700px)")
+myFunction(x) // Call listener function at run time
+x.addListener(myFunction) // Attach listener function on state changes
+
